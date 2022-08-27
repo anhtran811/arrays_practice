936a4e3a104b7b8c56a9a78fe54c4b1fdf25756b
Anhnhi Tran <anhtran811@gmail.com>
2022-08-27 13:25:54 -0600

    Added variables

---
+++ arrays.rb
@@ -0,0 +1,7 @@
+national_parks = ["Zion", "Banff", "Yosemite", "Grand Canyon"]
+years_visited = 2018, 2019, 2020, 2021
+travel_time = 9.5, 17.5, 15.5, 11.5
+drove_to_park = true
+did_not_drive = false
+camped = true
+did_not_camp = false
