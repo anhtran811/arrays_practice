46115611207ead31f26ea932b43cb9f336f799ca
Anhnhi Tran <anhtran811@gmail.com>
2022-08-27 14:56:27 -0600

    Ruby documentationstudy

--- arrays.rb
+++ arrays.rb
@@ -24,7 +24,7 @@ years_visited.pop
 years_visited.push 2022
 #! the method .shift will remove the first element 2018 from the Array of Strings in years_visited
 years_visited.shift
-#! the method .unshift will add the element 2017 at the front of the Array of Strings in years_visited
+#! the method .unshift will add the element 2017 at the front of the Array f Strings in years_visited
 years_visited.unshift 2017
 
 #! the method .pop will remove the last element 11.5 from the Array of Strings in travel_time
@@ -45,10 +45,15 @@ trip.shift
 #! the method .unshift will add the element false(tent_camp) at the front of the Array of Strings in trip
 trip.unshift tent_camp = false
 
-Index positions in an Array starts at 0.  It indicates the position in which to find the element and allows you to access the element.
-Example:  national_parks = ["Zion", "Banff", "Yosemite", "Grand Canyon"]
-index positions:
+#! Index Positions in an Array starts at 0.  It indicates the position in which to find the element and allows you to access the element.
+#! Example:  national_parks = ["Zion", "Banff", "Yosemite", "Grand Canyon"]
+#! index positions:
 "Zion" is [0]
 "Banff" is [1]
 "Yosemite" is [2]
 "Grand Canyon" is [3]
+
+#! Ruby Documentation: Accessing Elements
+#! this allows us to retrieve an element in an array by using [square brackets] around the index position
+#! Example: years_visited = [2018, 2019, 2020, 2021]
+#1 years_visited[2] will yield the output 2020 (index position 2)
