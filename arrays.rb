a87dba168b4edc1236af5ec30381d36c2e95b419
Anhnhi Tran <anhtran811@gmail.com>
2022-08-27 13:29:22 -0600

    Initial commit

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,8 @@
 national_parks = ["Zion", "Banff", "Yosemite", "Grand Canyon"]
-years_visited = 2018, 2019, 2020, 2021
-travel_time = 9.5, 17.5, 15.5, 11.5
+years_visited = [2018, 2019, 2020, 2021]
+travel_time = [9.5, 17.5, 15.5, 11.5]
 drove_to_park = true
-did_not_drive = false
+took_dogs= false
 camped = true
-did_not_camp = false
+hiked = false
+trip = [drove_to_park, took_dogs, camped, hiked]
