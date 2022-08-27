bd8a8ac29fbbdbebb3a5a083e454f40a34e52d89
Anhnhi Tran <anhtran811@gmail.com>
2022-08-27 13:30:23 -0600

    Added variables

--- arrays.rb
+++
@@ -1,8 +0,0 @@
-national_parks = ["Zion", "Banff", "Yosemite", "Grand Canyon"]
-years_visited = [2018, 2019, 2020, 2021]
-travel_time = [9.5, 17.5, 15.5, 11.5]
-drove_to_park = true
-took_dogs= false
-camped = true
-hiked = false
-trip = [drove_to_park, took_dogs, camped, hiked]
